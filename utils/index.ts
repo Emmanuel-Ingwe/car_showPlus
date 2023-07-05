1ebc6fe9fff6723e0cde59b778666452da021f15
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-06 00:29:04 +0100

    Api

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6a0418eb10msh4a86e4f14293564p1cd7b6jsn986b0d36db97',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
