f65a10b7b82af968c4c0f233346f00df14757b1c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-06 00:34:10 +0100

    __

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { headers } from "next/dist/client/components/headers";
+
 const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 const options = {
 	method: 'GET',
@@ -13,4 +15,15 @@ try {
 	console.log(result);
 } catch (error) {
 	console.error(error);
+}
+
+export async function fetchcars() {
+    const headers = {
+        'X-RapidAPI-Key': '6a0418eb10msh4a86e4f14293564p1cd7b6jsn986b0d36db97',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+
+const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla');
+
 }
