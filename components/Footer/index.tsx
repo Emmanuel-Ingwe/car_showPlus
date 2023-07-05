b29932295f0df52846eff7717ee52ee6add70d19
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-05 15:02:58 +0100

    links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { footerLinks } from '@/constants'
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+        <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+            <div className="flex flex-col justify-start items-start gap-6">
+                <Image src="/logo.svg" alt='logo' width={118} height={18} className='object-contain' />
+                <p className='text-base text-gray-700'>Carhub 2023 <br /> All rights reserved &copy;</p>
+            </div>
+
+            <div className="footer__links">
+                {footerLinks.map((link) => (
+                    <div key={link.title} className='footer__link'>
+                        <h3>{link.title}</h3>
+                        {link.links.map((item) => (
+                            <Link key={item.title} href={item.title} className='text-gray-500'>{item.title}</Link>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer 
