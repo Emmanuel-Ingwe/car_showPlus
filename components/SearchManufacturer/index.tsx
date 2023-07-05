5ea712a20c43283c4cf5e9810d4c29cf2965b7a4
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-05 23:59:34 +0100

    combo value

--- components/SearchManufacturer/index.tsx
+++ components/SearchManufacturer/index.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManufactur
 
   return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
             <Combobox.Button className="absolute top-[14px]">
                 <Image src="/car-logo.svg"
