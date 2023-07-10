fac128236b0d9f55368f8ae5807b686f2baab4b7
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-10 11:48:54 +0100

    searchbutn

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,9 +1,39 @@
 "use client"
 
+import Image from 'next/image'
+
 import React, { useState } from 'react'
 import SearchManufacturer from '../SearchManufacturer'
 
 import { SearchManufacturerProps } from '@/types' 
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
+
+// const SearchBar = () => {
+//   const [manufacturer, setManuFacturer] = useState("");
+//   const [model, setModel] = useState("");
+
+//   const router = useRouter();
+
+//   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     if (manufacturer.trim() === "" && model.trim() === "") {
+//       return alert("Please provide some input");
+//     }
+
+//     updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+//   };
 
 
 type Props = {}
@@ -22,6 +52,8 @@ const SearchBar = (props: Props) => {
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
             />
+
+            <SearchButton otherClasses='sm:hidden' />
         </div>
     </form>
   )
