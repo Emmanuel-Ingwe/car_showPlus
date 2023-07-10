012ff8e8efc0139f9f3d804ee76641050f4ca871
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-10 12:35:39 +0100

    router

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 import React, { useState } from 'react'
 import SearchManufacturer from '../SearchManufacturer'
@@ -28,6 +29,7 @@ type Props = {}
 const SearchBar = (props: Props) => {
     const [manufacturer, setManufacturer] = useState('')
     const [model, setModel] = useState('')
+    const router = useRouter()
 
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
           e.preventDefault();
@@ -36,6 +38,26 @@ const SearchBar = (props: Props) => {
              return alert("Please provide some input");
         }
       }
+
+      const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search)
+
+        if(model) {
+          searchParams.set('model', model)
+        } else {
+          searchParams.delete('model')
+        }
+
+      if(manufacturer) {
+        searchParams.set('manufacturer', manufacturer)
+      } else {
+        searchParams.delete('manufacturer')
+      }
+
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+      router.push(newPathname)
+    }
 
   return (
     <form action="" onSubmit={handleSearch} className="searchbar">
