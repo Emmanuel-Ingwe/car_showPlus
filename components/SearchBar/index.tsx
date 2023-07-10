1d89886e4d24b5dff463cca94b1d5315c32f589e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-10 12:15:45 +0100

    __

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -40,6 +40,7 @@ type Props = {}
 
 const SearchBar = (props: Props) => {
     const [manufacturer, setManufacturer] = useState('')
+    const [model, setModel] = useState('')
 
     const handleSearch = () => {
     }
@@ -55,6 +56,25 @@ const SearchBar = (props: Props) => {
 
             <SearchButton otherClasses='sm:hidden' />
         </div>
+        <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
   )
 }
