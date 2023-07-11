d7eb00d4c94722a3250969874a80485148f58f82
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-11 17:18:29 +0100

    filters

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchcars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': '6a0418eb10msh4a86e4f14293564p1cd7b6jsn986b0d36db97',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
 
-const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+const response = await fetch (`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers,
 });
  
